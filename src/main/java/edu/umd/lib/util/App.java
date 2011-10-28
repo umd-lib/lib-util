8576471ec4ed289ffcee3812c02d176eeacf30f0
Ben Wallberg <wallberg@umd.edu>
2011-10-28 13:44:38 +0000

    generate maven project using archetype
    mvn archetype:generate \
      -DarchetypeArtifactId=maven-archetype-quickstart \
      -DgroupId=edu.umd.lib \
      -DpackageName=edu.umd.lib \
      -Dpackage=edu.umd.lib.util \
      -DartifactId=lib-util \
      -Dversion=1.0-SNAPSHOT
    
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/lib-util@2430 983cd692-402e-0410-a370-846aaee464cf

---
+++ src/main/java/edu/umd/lib/util/App.java
@@ -0,0 +1,13 @@
+package edu.umd.lib.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
