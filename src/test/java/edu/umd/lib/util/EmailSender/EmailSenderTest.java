b10e2d574eeeb8ee58a9944ce175a6b64fda19ab
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2014-04-21 11:37:38 -0400

    LIBDC-745. fedora-utilities cleanup: reconcile EmailSender.
    EmailSenderTest update. mvn install fails on a test when attempting to connect to smtp server.
    
    https://issues.umd.edu/browse/LIBDC-745

--- src/test/java/edu/umd/lib/util/EmailSender/EmailSenderTest.java
+++ src/test/java/edu/umd/lib/util/EmailSender/EmailSenderTest.java
@@ -10,31 +10,24 @@ public class EmailSenderTest {
 	@Test
 	public void testPdfFileAttachment() throws MessagingException, IOException {
 		String[] content = {"This is a Test Email","Test encoding"," テイスド."};
-		/*this email address is private. should be replaced by tester's email address
-		*in order to receive test email.
-		**/
 		String[] recipients = {"jiangshaoxiong18@gmail.com"};
 		EmailSender sender = new EmailSender();
 		sender.setText(content);
 		sender.setSubject("This is a テイスド");
-		sender.setFile("src/test/resources/edu/umd/lib/util/testfile.txt");
-		sender.setHost("libsmtp.umd.edu", 87);
+		sender.setFile("src/test/java/edu/umd/lib/util/EmailSender/TestPDF.pdf");
+		sender.setHost("129.2.17.241", 87);
 		sender.sendEmail(recipients);
-		System.out.print("this");
 	}
 	
 	@Test
 	public void testTextFileAttachment() throws MessagingException, IOException {
 		String[] content = {"This is a Test Email","Test encoding"," テイスド."};
-		/*this email address is private. should be replaced by tester's email address
-		*in order to receive test email.
-		**/
 		String[] recipients = {"jiangshaoxiong18@gmail.com"};
 		EmailSender sender = new EmailSender();
 		sender.setText(content);
 		sender.setSubject("This is a テイスド");
-		sender.setFile("src/test/resources/edu/umd/lib/util/TestPDF.pdf");
-		sender.setHost("libsmtp.umd.edu", 87);
+		sender.setFile("src/test/java/edu/umd/lib/util/EmailSender/testfile.txt");
+		sender.setHost("129.2.17.241", 87);
 		sender.sendEmail(recipients);
 	}
 
